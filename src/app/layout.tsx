ee2448b5636048e0bcc135254e42132e550b7023
Jesus Villarreal <90210244+jesus0914@users.noreply.github.com>
2025-08-27 23:17:17 -0500

    aggregando funsiones resposi moviles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
                   },
                   mobile: {
                     position: 'br',
-                    xOffset: 0,
+                    xOffset: 10,
                     yOffset: 140
                   }
                 }
